2a1d2525e73c5f426af2c553359a45a8eb6ea340
Christopher Miller <admin@mctherealm.net>
2021-07-04 13:23:19 -0400

    Added about marker

--- src/util/cli.rs
+++ src/util/cli.rs
@@ -4,6 +4,7 @@ use crate::util::settings::GameFile;
 use crate::level::LevelId;
 
 pub const LOAD_ARG: &str = "load";
+const TEMPLE_URL: &str = "chrismiller.xyz";
 
 #[derive(Clone, Debug)]
 pub struct CliArgs {
@@ -12,10 +13,12 @@ pub struct CliArgs {
 
 pub fn get_cli_args(version: String, game_file: &GameFile) -> CliArgs {
   let author_list = game_file.authors.clone().into_iter().reduce(|acc, auth| acc + auth.as_str()).unwrap();
+  let about = format!("A game built on the Temple Platform ({})", TEMPLE_URL);
 
   let cli = App::new(game_file.title.as_str())
                   .version(version.as_str())
                   .author(author_list.as_str())
+                  .about(about.as_str())
                   .arg(Arg::with_name(LOAD_ARG)
                     .short("l")
                     .long("load")
