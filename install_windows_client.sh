3c35a0df1a009c77af118fd62838f909458be103
Christopher Miller <admin@mctherealm.net>
2021-11-06 12:07:02 -0400

    version bumped mingw lib location

--- install_windows_client.sh
+++ install_windows_client.sh
@@ -8,8 +8,8 @@ cargo install --locked --root install --path . --target x86_64-pc-windows-gnu
 mkdir -p windows
 cp install/bin/temple.exe windows/temple.exe
 cp -r assets windows
-cp /usr/lib/gcc/x86_64-w64-mingw32/8.3-posix/libstdc++-6.dll windows
-cp /usr/lib/gcc/x86_64-w64-mingw32/8.3-posix/libgcc_s_seh-1.dll windows
+cp /usr/lib/gcc/x86_64-w64-mingw32/9.3-posix/libstdc++-6.dll windows
+cp /usr/lib/gcc/x86_64-w64-mingw32/9.3-posix/libgcc_s_seh-1.dll windows
 cp /usr/x86_64-w64-mingw32/lib/libwinpthread-1.dll windows
 
 cd windows
