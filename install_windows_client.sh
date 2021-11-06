a4a94efb50c15c2d8c60d33f81d336f938593c36
Christopher Miller <admin@mctherealm.net>
2021-11-06 11:20:05 -0400

    Added windows building support

--- install_windows_client.sh
+++ install_windows_client.sh
@@ -3,14 +3,14 @@
 set -e
 
 rm -rf install
-cargo install --locked --root install --target x86_64-pc-windows-gnu
+cargo install --locked --root install --path . --target x86_64-pc-windows-gnu
 
 mkdir -p windows
 cp install/bin/temple.exe windows/temple.exe
-cp -r temple/assets windows
-cp /usr/lib/gcc/x86_64-w64-mingw32/8.3-posix/libstdc++-6.dll windows
-cp /usr/lib/gcc/x86_64-w64-mingw32/8.3-posix/libgcc_s_seh-1.dll windows
-cp /usr/x86_64-w64-mingw32/lib/libwinpthread-1.dll windows
+cp -r assets windows
+#cp /usr/lib/gcc/x86_64-w64-mingw32/8.3-posix/libstdc++-6.dll windows
+#cp /usr/lib/gcc/x86_64-w64-mingw32/8.3-posix/libgcc_s_seh-1.dll windows
+#cp /usr/x86_64-w64-mingw32/lib/libwinpthread-1.dll windows
 
 cd windows
-zip -r ../client/windows$VERSION.zip .
+zip -r ../windows$VERSION.zip .
