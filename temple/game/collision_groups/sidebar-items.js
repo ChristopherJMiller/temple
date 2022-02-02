49f56710f46ca729d59826b4cb7175a460645cbf
ChristopherJMiller <cmiller548@gmail.com>
2022-02-02 21:22:10 +0000

    Deploying to gh-pages from @ ChristopherJMiller/temple@d42d73e8176d8f31fd185626df8cdeb2000a7ae9 🚀

--- temple/game/collision_groups/sidebar-items.js
+++ temple/game/collision_groups/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["DETECTS_PLAYER_GROUP",""],["NONE_GROUP",""],["PLAYER_GROUP",""],["PLAYER_HOVER_GROUP",""],["SOLID_GROUP",""]]});
+initSidebarItems({"constant":[["DEADLY_GROUP",""],["DETECTS_PLAYER_GROUP",""],["NONE_GROUP",""],["PLAYER_GROUP",""],["PLAYER_HOVER_GROUP",""],["SOLID_GROUP",""]]});
