b0c342246a6f13bcef42f99203c206761c314fa9
ChristopherJMiller <hello@chrismiller.xyz>
2022-02-11 18:01:34 +0000

    Deploying to gh-pages from @ ChristopherJMiller/temple@d42047b95c906bfaea762788448d284385d50765 🚀

--- temple/game/sfx/sidebar-items.js
+++ temple/game/sfx/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["CHECKPOINT_SFX",""],["JUMP_SFX","Jump sound fx path"]],"fn":[["init_channel_volume",""],["load_sfx",""]],"struct":[["AudioChannels",""],["ChannelState",""],["SfxHandles",""],["SfxPlugin","[Plugin] for sfx startup."]]});
+initSidebarItems({"constant":[["CHECKPOINT_SFX",""],["JUMP_SFX","Jump sound fx path"]],"fn":[["init_channel_volume",""],["load_sfx",""],["update_volumes",""]],"struct":[["AudioChannels",""],["ChannelState",""],["SfxHandles",""],["SfxPlugin","[Plugin] for sfx startup."]]});
